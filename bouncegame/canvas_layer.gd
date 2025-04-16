03fad1dde1990f9cd5dbd153d2a6540cc9742501
1522637 <1522637@sd61learn.ca>
2025-04-16 14:14:23 -0700

    I made the winning peg

--- bouncegame/canvas_layer.gd
+++ bouncegame/canvas_layer.gd
@@ -1,14 +1,14 @@
 extends CanvasLayer
 var score = 0
 var lives = 10
+var winning_peg_left = 0
 func _ready():
 	pass
 func _process(delta):
-	var bottom_node = get_node("bottom")
 
 	$Points.text = "Points: " + str(score)
 	$balls.text = "Balls " + str(lives)
-
+	$pegs.text = "pegs: " + str(winning_peg_left)
 
 func _on_normal_peg_addpoint() -> void:
 	score = score + 100
--- bouncegame/normal_peg.gd
+++ bouncegame/normal_peg.gd
@@ -1,22 +1,33 @@
 extends Node2D
 
 signal addpoint
+@export var winning_peg_gen = 10
+var chosen_winning_peg = false
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	$winning.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 func _on_area_2d_body_entered(body):
+	var peg_left = get_node("/root/playtest/cannon/HUD") 
 	if body.is_in_group("balls"):
 		emit_signal("addpoint")
 		$Timer.start()
-	
-
-
+		if chosen_winning_peg == true:
+			peg_left.winning_peg_left -= 1
+			
 func _on_timer_timeout():
 	queue_free()
+
+func _on_winning_hidden() -> void: # changing the normal peg 
+	var peg_left = get_node("/root/playtest/cannon/HUD") 
+	if randi_range(1,3) == 2:
+		$winning.visible = true
+		$normal.visible = false
+		chosen_winning_peg = true
+		peg_left.winning_peg_left += 1
