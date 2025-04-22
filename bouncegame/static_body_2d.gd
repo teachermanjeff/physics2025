eb9a89befd644928ec83e4fa6f9ecc9fd62e2b80
SeunBello <belloseun2007@gmail.com>
2025-04-22 14:43:08 -0700

    updatedcannonsound

--- bouncegame/static_body_2d.gd
+++ bouncegame/static_body_2d.gd
@@ -5,6 +5,7 @@ func _process(delta):
 	var HUD_node = get_node("HUD") 
 	look_at(get_global_mouse_position())
 	if Input.is_action_just_pressed("left click") and bottom_node.is_shooting == false and HUD_node.lives > 0:
+		$cannonsound.play()
 		var new_projectile = projectile.instantiate()
 		get_parent().add_child(new_projectile)
 		HUD_node.lives -= 1
