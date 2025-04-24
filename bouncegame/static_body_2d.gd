bff284b93ee671e6fa92addacda24d037137c7f8
SeunBello <belloseun2007@gmail.com>
2025-04-24 14:48:13 -0700

    still added cannon

--- bouncegame/static_body_2d.gd
+++ bouncegame/static_body_2d.gd
@@ -1,10 +1,12 @@
 extends StaticBody2D
+@onready var bottom_node = $"../bottom"
+
+
 var projectile = preload("res://ball.tscn")
 func _process(delta):
-	var bottom_node = get_node("/root/playtest/bottom") 
 	var HUD_node = get_node("HUD") 
 	look_at(get_global_mouse_position())
-	if Input.is_action_just_pressed("left click") and bottom_node.is_shooting == false and HUD_node.lives > 0:
+	if Input.is_action_just_pressed("left click") and bottom_node.is_shooting == false and HUD_node.lives > 0: 
 		$cannonsound.play()
 		var new_projectile = projectile.instantiate()
 		get_parent().add_child(new_projectile)
