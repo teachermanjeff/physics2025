80dd7feabf20acd89da6935d4f36479bf69aa377
Abe-Kirkland <Abek4703@gmail.com>
2025-04-28 10:02:26 -0700

    controlling with wasd

--- bouncegame/ball.gd
+++ bouncegame/ball.gd
@@ -1,9 +1,24 @@
 extends RigidBody2D
-func _ready() -> void:
-	launch()
+var aim_pos: Vector2
+var x_aim = 0
+var y_aim: float
 func launch():
 	var velocity : Vector2 =  300 * _get_forward()
 	apply_central_impulse(velocity)
 func _get_forward() -> Vector2:
-	var test = global_position.direction_to(get_global_mouse_position())
+	var test = global_position.direction_to(aim_pos)
 	return test
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("left"):
+		if x_aim > -1000:
+			x_aim -= 10
+	if Input.is_action_pressed("right"):
+		if x_aim < 1000:
+			x_aim += 10
+	y_aim = (sqrt(1000000-(x_aim)**2))
+	aim_pos = Vector2(x_aim,y_aim)
+	print(aim_pos)
+func _ready() -> void:
+	launch()
+	
+	
--- bouncegame/rotator.gd
+++ bouncegame/rotator.gd
@@ -1,8 +1,11 @@
 extends Node2D
+var aim_pos: Vector2
+var x_aim = 0
+var y_aim: float
 func _draw() -> void:
 	_update_traj()
 func _get_forward() -> Vector2:
-	var test = global_position.direction_to(get_global_mouse_position())
+	var test = global_position.direction_to(aim_pos)
 	return test
 func _update_traj() -> void:
 	var velocity : Vector2 =  300 * _get_forward()
@@ -18,4 +21,12 @@ func _update_traj() -> void:
 		draw_dashed_line(line_start, line_end, colour, 1)
 		line_start = line_end
 func _process(delta: float) -> void:
+	if Input.is_action_pressed("left"):
+		if x_aim > -1000:
+			x_aim -= 10
+	if Input.is_action_pressed("right"):
+		if x_aim < 1000:
+			x_aim += 10
+	y_aim = (sqrt(1000000-(x_aim)**2))
+	aim_pos = Vector2(x_aim,y_aim)
 	queue_redraw()
