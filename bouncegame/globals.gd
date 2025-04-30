6c934e680fc09fe1721789ff427009f0830e5aa3
Abe-Kirkland <Abek4703@gmail.com>
2025-04-30 10:03:32 -0700

    fixed the cannon so it works with touchscreen

--- bouncegame/globals.gd
+++ bouncegame/globals.gd
@@ -1,6 +1,6 @@
 extends Node
 var x_aim = 0
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if Input.is_action_pressed("left"):
 		if x_aim > -1000:
 			x_aim -= 10
--- bouncegame/static_body_2d.gd
+++ bouncegame/static_body_2d.gd
@@ -1,12 +1,16 @@
 extends StaticBody2D
 @onready var bottom_node = $"../bottom"
+var y_aim: int
+var aim_pos: Vector2
 
 
 var projectile = preload("res://ball.tscn")
 func _process(delta):
-	var HUD_node = get_node("HUD") 
-	look_at(get_global_mouse_position())
-	if Input.is_action_just_pressed("Shoot") and bottom_node.is_shooting == false and HUD_node.lives > 0: 
+	var HUD_node = get_node("HUD")
+	y_aim = (sqrt(1000000-(globals.x_aim)**2))
+	aim_pos = Vector2(globals.x_aim,y_aim) 
+	look_at(aim_pos)
+	if Input.is_action_just_pressed("shoot") and bottom_node.is_shooting == false and HUD_node.lives > 0: 
 		$cannonsound.play()
 		var new_projectile = projectile.instantiate()
 		get_parent().add_child(new_projectile)
