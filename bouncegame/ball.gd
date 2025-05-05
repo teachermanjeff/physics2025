aba620055a52ef2ad6bbec4fba8b6f2702bbcd7a
1522637 <1522637@sd61learn.ca>
2025-05-05 13:29:49 -0700

    fixed the ball getting stuck

--- bouncegame/ball.gd
+++ bouncegame/ball.gd
@@ -1,14 +1,22 @@
 extends RigidBody2D
 var aim_pos: Vector2
+var velocity : Vector2 =  300 * _get_forward()
+var gravity = Vector2(0,1) 
 func launch():
 	var y_aim = (sqrt(1000000-(globals.x_aim)**2))
 	aim_pos = Vector2(globals.x_aim,y_aim)
-	var velocity : Vector2 =  300 * _get_forward()
+	velocity =  300 * _get_forward()
 	apply_central_impulse(velocity)
+	
 func _get_forward() -> Vector2:
 	var test = global_position.direction_to(aim_pos)
 	return test
+	
 func _ready() -> void:
 	launch()
+
+func _process(delta):
+	apply_central_impulse(gravity)
+	
 	
 	
