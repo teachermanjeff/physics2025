acc4bfba03db955e580b17bf731e3771f7d01d93
1522637 <1522637@sd61learn.ca>
2025-05-06 14:51:47 -0700

    ahhhhh

--- bouncegame/bottom.gd
+++ bouncegame/bottom.gd
@@ -1,6 +1,4 @@
 extends Area2D
-
 var is_shooting = false
-
 func _on_body_entered(body: Node2D) -> void:
 	is_shooting = false
--- bouncegame/canvas_layer.gd
+++ bouncegame/canvas_layer.gd
@@ -3,13 +3,14 @@ var score = 0
 var lives = 10
 var winning_peg_left = 0
 var won = true
+var lost = true
 func _ready():
 	$losing_screen.visible = false
 	pass
 func _process(delta):
-	if won == false and lives <= 0:
-		$losing_screen.visible = true
-		$try_again.visible = true
+	if lives <= 0 and lost == true:
+		$Timer.start()
+		lost = false
 	$Points.text = "Points: " + str(score)
 	$balls.text = "Balls " + str(lives)
 	$pegs.text = "pegs: " + str(winning_peg_left)
@@ -26,3 +27,7 @@ func _on_normal_peg_addpoint() -> void:
 func _on_try_again_pressed() -> void:
 	get_tree().reload_current_scene()
 	
+func _on_timer_timeout() -> void:
+	if won == false and lives <= 0:
+		$losing_screen.visible = true
+		$try_again.visible = true
