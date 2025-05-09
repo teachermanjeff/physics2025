d3103e90efd95fef2e359642558b5648baaf133e
RichardPolivka <147742779+RichardPolivka@users.noreply.github.com>
2025-05-09 12:41:52 -0700

    EEE

--- bouncegame/playbutton.gd
+++ bouncegame/playbutton.gd
@@ -3,13 +3,3 @@ extends Button
 
 func _on_pressed():
 	get_tree().change_scene_to_file("res://playtest.tscn")
-
-
-
-
-func _on_playbutton_2_pressed() -> void:
-	get_tree().change_scene_to_file("res://level1.tscn")
-
-
-func _on_playbutton_3_pressed() -> void:
-	get_tree().change_scene_to_file("res://level_2.tscn")
